15a9d1497cdcb2e24597de90f5ce5cd68c0bf680
lana-e <lana.elauria@berkeley.edu>
2022-04-12 17:38:43 -0700

    cleaning up & Flask running locally

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
+# read file
+with open('./static/dashboard-lana.json', 'r') as spec_lana:
+    dash_lana = spec_lana.read()
+
+with open('./static/dashboard-li.json', 'r') as spec_li:
+    dash_li = spec_li.read()
+
+
+# @app.route("/")
+# def index():
+#     return render_template('index.html', title="page", jsonfile=json.dumps(lana_dash))
+
 @app.route("/")
-def w209():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", dash_lana=dash_lana, dash_li=dash_li)
 
 if __name__ == "__main__":
     app.run()
-    
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
